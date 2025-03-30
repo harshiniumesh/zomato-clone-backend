0a87ace510f02070d6411149fdcee86fd5631171
harshiniumesh <harshinividya127@gmail.com>
2025-03-30 16:29:27 +0530

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const userControllers = require("../controllers/userControllers");
+
+const {
+  getUserDetails,
+  updateUserDetails,
+  addToBookmarks,
+  addToCart,
+  getUserCart,
+  removeFromCart,
+  removeFromBookmarks,
+  getBookmarks,
+  clearCart,
+} = userControllers;
+
+router.route("/details").get(getUserDetails);
+router.route("/update-details").put(updateUserDetails);
+router.route("/add-to-cart").post(addToCart);
+router.route("/get-cart/:userId").get(getUserCart);
+router.route("/clear-cart/:userId").post(clearCart);
+router.route("/remove-from-cart").delete(removeFromCart);
+router.route("/add-to-bookmarks").post(addToBookmarks);
+router.route("/get-bookmarks").get(getBookmarks);
+router.route("/remove-from-bookmarks").delete(removeFromBookmarks);
+
+module.exports = router;
